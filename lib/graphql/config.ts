0af8f2873a075d339aac6f97bc77d512ac449605
Liquidibrium <vanoganjelashvili@gmail.com>
2023-05-30 12:04:13 +0400

    add eas build

--- lib/graphql/config.ts
+++ lib/graphql/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://192.168.70.164:8080/graphql';
-// export const API_URL = 'https://car-app-api.up.railway.app/graphql';
+// export const API_URL = 'http://192.168.70.164:8080/graphql';
+export const API_URL = 'https://car-app-api.up.railway.app/graphql';
