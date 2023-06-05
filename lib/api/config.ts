8476f64acd8108647f6112219fa33d904ff7d3b1
Liquidibrium <vanoganjelashvili@gmail.com>
2023-06-05 12:07:20 +0400

    Update api url

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,5 +1,5 @@
 // export const HTTP_API_URL = 'http://192.168.193.164:8080';
 // export const WS_API_URL = 'ws://192.168.193.164:8080';
 
-export const HTTP_API_URL = 'https://car-app-api.up.railway.app';
-export const WS_API_URL = 'wss://car-app-api.up.railway.app';
+export const HTTP_API_URL = 'https://api.liquidibrium.me';
+export const WS_API_URL = 'wss://api.liquidibrium.me';
