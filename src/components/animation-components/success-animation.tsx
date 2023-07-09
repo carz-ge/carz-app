6c28a3d3180a63eff43e8811df25c098658b3da5
Liquidibrium <vanoganjelashvili@gmail.com>
2023-07-10 01:29:21 +0400

    Added animations

---
+++ src/components/animation-components/success-animation.tsx
@@ -0,0 +1,90 @@
+import {View, StyleSheet} from 'react-native';
+import React from 'react';
+import LottieView, {AnimationObject} from 'lottie-react-native';
+import colors from '../../styles/colors';
+export default function SuccessAnimation() {
+  const localSource =
+    require('../../animations/success.json') as AnimationObject;
+
+  return (
+    <View style={styles.container}>
+      <LottieView
+        key={`success`}
+        source={localSource}
+        autoPlay={true}
+        loop={false}
+        style={styles.lottie}
+        resizeMode={'contain'}
+        colorFilters={colorFilter}
+        enableMergePathsAndroidForKitKatAndAbove
+        onAnimationFinish={() => {
+          console.log('finished');
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 32,
+  },
+  controlsContainer: {flexDirection: 'row', marginTop: 24, columnGap: 12},
+  button: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+  },
+  text: {color: 'white'},
+  lottie: {width: 400, height: 400},
+});
+
+const colorFilter = [
+  {
+    keypath: 'BG',
+    color: colors.primary,
+  },
+  {
+    keypath: 'O-B',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'L-B',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'T1a-Y 2',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'T1b-Y',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'T2b-B',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'T2a-B',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'I-Y',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'E1-Y',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'E2-Y',
+    color: colors.secondary,
+  },
+  {
+    keypath: 'E3-Y',
+    color: colors.secondary,
+  },
+];
--- src/screens/payment/payment-status.tsx
+++ src/screens/payment/payment-status.tsx
@@ -1,10 +1,18 @@
 import {RootStackScreenProps} from '../../navigation/types';
 import React from 'react';
 import {Text} from 'react-native';
+import SuccessAnimation from '../../components/animation-components/success-animation';
+import FailAnimation from '../../components/animation-components/fail-animation';
 export default function PaymentStatusScreen({
   route,
   navigation,
 }: RootStackScreenProps<'paymentStatus'>) {
   console.log('is Success full-> ', route.params.success, route.params.orderId);
-  return <Text>route.params?.success</Text>;
+  const success = route.params?.success || false;
+  return (
+    <>
+      <Text>{success}</Text>;
+      {success ? <SuccessAnimation /> : <FailAnimation />}
+    </>
+  );
 }
