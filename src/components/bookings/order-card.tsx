2c192250cd17033664b478819b2667b02d8f7bd0
vano <vano@theneo.io>
2023-07-15 20:43:15 +0400

    added day-fns

--- src/components/bookings/order-card.tsx
+++ src/components/bookings/order-card.tsx
@@ -1,33 +1,51 @@
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import {Order} from '../../graphql/operations';
 import colors from '../../styles/colors';
 import React from 'react';
 import {Ionicons} from '@expo/vector-icons';
 import {convertPriceIntoGel} from '../../utils/price';
 
-import dayjs from 'dayjs';
 import {getLocales, getCalendars} from 'expo-localization';
+
+import {format, formatDistance, formatRelative} from 'date-fns';
+import {ka} from 'date-fns/locale';
+
+import dayjs from 'dayjs';
 import 'dayjs/locale/ka';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import localeData from 'dayjs/plugin/localeData';
 import updateLocale from 'dayjs/plugin/updateLocale';
+
 import Moment from 'moment';
+import {useNavigation} from '@react-navigation/native';
+import {MainTabParamList} from '../../navigation/types';
+import {NavigationProp} from '@react-navigation/core/src/types';
 
 const locale = getLocales();
 const cal = getCalendars();
+
 console.log(locale, cal);
 dayjs.extend(updateLocale);
 dayjs.locale('ka');
 dayjs.apply(localeData);
 dayjs.apply(localizedFormat);
+
 Moment.locale('ka');
+
 interface OrderCardProps {
   order: Order;
 }
 
 export default function OrderCard({order}: OrderCardProps) {
+  const navigation = useNavigation<NavigationProp<MainTabParamList>>();
+  const date = Date.parse(order.updatedAt || '');
   return (
-    <View>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('product', {
+          productId: order.product.id,
+        });
+      }}>
       <View style={styles.detailsContainer}>
         <Text>{order.productPackage.name.ka}</Text>
         <Text>{order.product.name.ka}</Text>
@@ -40,6 +58,22 @@ export default function OrderCard({order}: OrderCardProps) {
         <Text>{order.carPlateNumber}</Text>
         <Text>{dayjs(order.createdAt).toString()}</Text>
         <Text>{Moment(order.updatedAt).format('LLLL').toLocaleString()}</Text>
+        <Text>
+          {formatRelative(date, new Date(), {
+            locale: ka,
+          })}
+        </Text>
+        <Text>
+          {format(date, 'HH:MM MMMM yyyy', {
+            locale: ka,
+            weekStartsOn: 0,
+          })}
+        </Text>
+        <Text>
+          {formatDistance(date, new Date(), {
+            locale: ka,
+          })}
+        </Text>
         <Text>{order.errorMessage}</Text>
         {order.product.location && (
           <View
@@ -62,7 +96,7 @@ export default function OrderCard({order}: OrderCardProps) {
           source={{uri: order.product.mainImage}}
         />
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
